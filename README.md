055da65e7a562d17a73aff7dc95a883e98049662
anubhavrawal <rawal.anubhav@outlook.com>
2021-04-23 11:25:05 -0400

    Updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# HeapSim
+# Heap Simulator
+
+## Compile instruction
+
+```
+make run
+```
+
+OR
+
+```
+python3 hsim.py [-v] [-d] --free-list={implicit or explicit} --fit={first or best} <input file>
+```
+
+### Required flags:
+    --fit= {first/best}
+    --free-list={implicit/explicit}
+
+### Usable optional flags:
+    -h prints usage
+    -v is for verbose mode
+    -d will prevent from creating a `output.txt` file and display on screen
+    -s <value> will allow to change the size of initial heap 
+    -w writes the verbose commands into a file and stores into the `verbose` folder
+
+`output.txt` file is creadted on deafult to show the state of the heap.
+> `output.txt` will not be created if there is the error on the code.
+
+
+## Notes on Explicit first and best fit:
+For these conditions fist footer is collased then header is collased and the new pointers will not be assigned until both collasing are checked and/or completed
+
--- exp_first.mk
+++ exp_first.mk
@@ -1,73 +1,73 @@
 all: test1 test2 test3 test4 test5 test6 test7 test8 test9 test10 test11 test12 test13
 
 compile1:
-	python3 main.py --free-list=explicit --fit=first examples/examples/1/in.txt 
+	python3 hsim.py --free-list=explicit --fit=first examples/examples/1/in.txt 
 
 test1: compile1
 	diff output.txt examples/examples/1/explicit.first.txt
 
 compile2:
-	python3 main.py --free-list=explicit --fit=first examples/examples/2/in.txt 
+	python3 hsim.py --free-list=explicit --fit=first examples/examples/2/in.txt 
 
 test2: compile2
 	diff output.txt examples/examples/2/explicit.first.txt
 
 compile3:
-	python3 main.py --free-list=explicit --fit=first examples/examples/3/in.txt 
+	python3 hsim.py --free-list=explicit --fit=first examples/examples/3/in.txt 
 
 test3:compile3
 	diff output.txt examples/examples/3/explicit.first.txt
 
 compile4:
-	python3 main.py --free-list=explicit --fit=first examples/examples/4/in.txt 
+	python3 hsim.py --free-list=explicit --fit=first examples/examples/4/in.txt 
 
 test4: compile4
 	diff output.txt examples/examples/4/explicit.first.txt
 
 compile5:
-	python3 main.py --free-list=explicit --fit=first examples/examples/5/in.txt 
+	python3 hsim.py --free-list=explicit --fit=first examples/examples/5/in.txt 
 
 test5: compile5
 	diff output.txt examples/examples/5/explicit.first.txt
 
 compile6:
-	python3 main.py --free-list=explicit --fit=first examples/examples/6/in.txt 
+	python3 hsim.py --free-list=explicit --fit=first examples/examples/6/in.txt 
 
 test6:compile6
 	diff output.txt examples/examples/6/explicit.first.txt
 
 compile7:
-	python3 main.py --free-list=explicit --fit=first examples/examples/7/in.txt 
+	python3 hsim.py --free-list=explicit --fit=first examples/examples/7/in.txt 
 
 test7:compile7
 	diff output.txt examples/examples/7/explicit.first.txt
 
 compile8:
-	python3 main.py --free-list=explicit --fit=first examples/examples/8/in.txt 
+	python3 hsim.py --free-list=explicit --fit=first examples/examples/8/in.txt 
 
 test8: compile8
 	diff output.txt examples/examples/8/explicit.first.txt
 
 compile10:
-	python3 main.py --free-list=explicit --fit=first examples/examples/10/in.txt 
+	python3 hsim.py --free-list=explicit --fit=first examples/examples/10/in.txt 
 
 test10:compile10
 	diff output.txt examples/examples/10/explicit.first.txt
 
 compile11:
-	python3 main.py --free-list=explicit --fit=first examples/examples/11/in.txt 
+	python3 hsim.py --free-list=explicit --fit=first examples/examples/11/in.txt 
 
 test11:compile11
 	diff output.txt examples/examples/11/explicit.first.txt
 
 compile12:
-	python3 main.py --free-list=explicit --fit=first examples/examples/12/in.txt 
+	python3 hsim.py --free-list=explicit --fit=first examples/examples/12/in.txt 
 
 test12:compile12
 	diff output.txt examples/examples/12/explicit.first.txt
 
 compile13:
-	python3 main.py --free-list=explicit --fit=first examples/examples/13/in.txt 
+	python3 hsim.py --free-list=explicit --fit=first examples/examples/13/in.txt 
 
 test13:compile13
 	diff output.txt examples/examples/13/explicit.first.txt
