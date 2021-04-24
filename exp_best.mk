7a220fbae0e1fc7e659ca04869da7ef49820814d
anubhavrawal <rawal.anubhav@outlook.com>
2021-04-24 12:50:30 -0400

    Revet backed the test cases

--- exp_best.mk
+++ exp_best.mk
@@ -1,73 +1,73 @@
 all: test1 test2 test3 test4 test5 test6 test7 test8 test10 test11 test12 test13
 
 compile1:
-	python3 hsim.py --free-list=explicit --fit=best examples/examples/1/in.txt
+	python3 hsim.py --free-list=explicit --fit=best examples/1/in.txt
 
 test1: compile1
-	diff output.txt examples/examples/1/explicit.best.txt
+	diff output.txt examples/1/explicit.best.txt
 
 compile2:
-	python3 hsim.py --free-list=explicit --fit=best examples/examples/2/in.txt
+	python3 hsim.py --free-list=explicit --fit=best examples/2/in.txt
 
 test2: compile2
-	diff output.txt examples/examples/2/explicit.best.txt
+	diff output.txt examples/2/explicit.best.txt
 
 compile3:
-	python3 hsim.py --free-list=explicit --fit=best examples/examples/3/in.txt
+	python3 hsim.py --free-list=explicit --fit=best examples/3/in.txt
 
 test3:compile3
-	diff output.txt examples/examples/3/explicit.best.txt
+	diff output.txt examples/3/explicit.best.txt
 
 compile4:
-	python3 hsim.py --free-list=explicit --fit=best examples/examples/4/in.txt
+	python3 hsim.py --free-list=explicit --fit=best examples/4/in.txt
 
 test4: compile4
-	diff output.txt examples/examples/4/explicit.best.txt
+	diff output.txt examples/4/explicit.best.txt
 
 compile5:
-	python3 hsim.py --free-list=explicit --fit=best examples/examples/5/in.txt
+	python3 hsim.py --free-list=explicit --fit=best examples/5/in.txt
 
 test5: compile5
-	diff output.txt examples/examples/5/explicit.best.txt
+	diff output.txt examples/5/explicit.best.txt
 
 compile6:
-	python3 hsim.py --free-list=explicit --fit=best examples/examples/6/in.txt
+	python3 hsim.py --free-list=explicit --fit=best examples/6/in.txt
 
 test6:compile6
-	diff output.txt examples/examples/6/explicit.best.txt
+	diff output.txt examples/6/explicit.best.txt
 
 compile7:
-	python3 hsim.py --free-list=explicit --fit=best examples/examples/7/in.txt
+	python3 hsim.py --free-list=explicit --fit=best examples/7/in.txt
 
 test7:compile7
-	diff output.txt examples/examples/7/explicit.best.txt
+	diff output.txt examples/7/explicit.best.txt
 
 compile8:
-	python3 hsim.py --free-list=explicit --fit=best examples/examples/8/in.txt
+	python3 hsim.py --free-list=explicit --fit=best examples/8/in.txt
 
 test8: compile8
-	diff output.txt examples/examples/8/explicit.best.txt
+	diff output.txt examples/8/explicit.best.txt
 
 compile10:
-	python3 hsim.py --free-list=explicit --fit=best examples/examples/10/in.txt
+	python3 hsim.py --free-list=explicit --fit=best examples/10/in.txt
 
 test10:compile10
-	diff output.txt examples/examples/10/explicit.best.txt
+	diff output.txt examples/10/explicit.best.txt
 
 compile11:
-	python3 hsim.py --free-list=explicit --fit=best examples/examples/11/in.txt
+	python3 hsim.py --free-list=explicit --fit=best examples/11/in.txt
 
 test11:compile11
-	diff output.txt examples/examples/11/explicit.best.txt
+	diff output.txt examples/11/explicit.best.txt
 
 compile12:
-	python3 hsim.py --free-list=explicit --fit=best examples/examples/12/in.txt
+	python3 hsim.py --free-list=explicit --fit=best examples/12/in.txt
 
 test12:compile12
-	diff output.txt examples/examples/12/explicit.best.txt
+	diff output.txt examples/12/explicit.best.txt
 
 compile13:
-	python3 hsim.py --free-list=explicit --fit=best examples/examples/13/in.txt
+	python3 hsim.py --free-list=explicit --fit=best examples/13/in.txt
 
 test13:compile13
-	diff output.txt examples/examples/13/explicit.best.txt
+	diff output.txt examples/13/explicit.best.txt
